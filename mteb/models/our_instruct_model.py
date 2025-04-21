9e788edbd0ea2775f253f12ae4b0ad79229a185f
mehran <mehan.sarmadi16@gmail.com>
2025-04-21 12:05:18 +0330

    add retro_9neg_instruct_stage3_v2_v6_with_inbatch

--- mteb/models/our_instruct_model.py
+++ mteb/models/our_instruct_model.py
@@ -782,3 +782,30 @@ ourwordpiece_retro_balance_small = ModelMeta(
     training_datasets={},  # English part of gte_multi_training_data,
     open_weights=True
 )
+
+
+our_model_9 = ModelMeta(
+    loader=partial(  
+        OurModelWrapper,
+        trust_remote_code=True,
+        model_name = "retro_9neg_instruct_stage3_v2_v6_with_inbatch2",
+        revision="v1",
+    ),
+    name = "retro_9neg_instruct_stage3_v2_v6_with_inbatch",
+    languages=["fas_Arab"],
+    revision="v1",
+    release_date="2024-02-10",
+    n_parameters=int(149 * 1e6),
+    memory_usage_mb=284,
+    embed_dim=768,
+    license="apache-2",
+    max_tokens=8192,
+    reference="https://huggingface.co/Alibaba-NLP/gte-modernbert-base",
+    similarity_fn_name="cosine",
+    framework=["Sentence Transformers", "PyTorch"],
+    use_instructions=False,
+    public_training_code=None,  # couldn't find
+    public_training_data=None,
+    training_datasets={},  # English part of gte_multi_training_data,
+    open_weights=True
+)
