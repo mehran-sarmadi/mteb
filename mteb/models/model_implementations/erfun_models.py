cf79ea64300a3d6fd9c4f2ba725680b80b6946ee
mehran-sarmadi <mehran.sarmadi16@gmail.com>
2025-09-24 11:44:55 +0330

    add new models

--- mteb/models/model_implementations/erfun_models.py
+++ mteb/models/model_implementations/erfun_models.py
@@ -936,3 +936,57 @@ model_21 = ModelMeta(
     public_training_data=None,
     training_datasets=None,
 )
+
+############# test with prompt #############
+model_22 = ModelMeta(
+    loader=partial(
+        HakimModelWrapper,
+        trust_remote_code=True,
+        model_name="/mnt/data/ez-workspace/FlagEmbedding/FlagEmbedding/baai_general_embedding/results/hakim_instruct_stage2_v2_v7_with_inbatch_long_2048_1e-6",
+        revision="v1",
+    ),
+    name="erfun/hakim_instruct_stage2_v2_v7_with_inbatch_long_2048_1e-6",
+    languages=["fas-Arab"],
+    open_weights=False,
+    revision="1",
+    release_date="2025-05-10",
+    n_parameters=124_441_344,
+    memory_usage_mb=475,
+    embed_dim=768,
+    license="not specified",
+    max_tokens=512,
+    reference="https://huggingface.co/MCINext/Hakim-unsup",
+    similarity_fn_name="cosine",
+    framework=["API"],
+    use_instructions=False,
+    public_training_code=None,
+    public_training_data=None,
+    training_datasets=None,
+)
+
+############# test with prompt #############
+model_23 = ModelMeta(
+    loader=partial(
+        HakimModelWrapper,
+        trust_remote_code=True,
+        model_name="/mnt/data/ez-workspace/FlagEmbedding/FlagEmbedding/baai_general_embedding/results/hakim_instruct_stage2_v2_v7_with_inbatch_long_2048_ds/checkpoint-47054",
+        revision="v1",
+    ),
+    name="erfun/hakim_instruct_stage2_v2_v7_with_inbatch_long_2048_ds",
+    languages=["fas-Arab"],
+    open_weights=False,
+    revision="1",
+    release_date="2025-05-10",
+    n_parameters=124_441_344,
+    memory_usage_mb=475,
+    embed_dim=768,
+    license="not specified",
+    max_tokens=512,
+    reference="https://huggingface.co/MCINext/Hakim-unsup",
+    similarity_fn_name="cosine",
+    framework=["API"],
+    use_instructions=False,
+    public_training_code=None,
+    public_training_data=None,
+    training_datasets=None,
+)
